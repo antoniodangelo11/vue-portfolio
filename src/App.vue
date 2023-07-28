f8238bd49f1cfe32814b32bb16e3c8f894a3503a
Antonio D'Angelo <reapercsp@gmail.com>
2023-07-28 12:17:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+  import { onMounted } from "vue";
+  import { initFlowbite } from "flowbite";
+
+  // initialize components based on data attribute selectors
+  onMounted(() => {
+    initFlowbite();
+  });
+  
+  export default {};
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline text-red-500">
+    Hello world!
+  </h1>
+  <i class="fa-brands fa-facebook"></i>
+</template>
+
+<style lang="scss">
+</style>
