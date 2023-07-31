bc1660fcedc0bf31b0d67689e852def06a2d6657
Paolo96700 <paolo.falcoapp@gmail.com>
2023-07-31 13:16:33 +0200

    mod - footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,7 +4,7 @@
 
 <template>
     <footer class="">
-        <div class="container flex flex-col p-4 mx-auto md:p-8 lg:flex-row divide-gray-400">
+        <div class="container flex flex-col p-4 mx-auto md:p-3 lg:flex-row divide-gray-400">
             <ul
                 class="self-center py-6 space-y-4 text-center sm:flex sm:space-y-0 sm:justify-around sm:space-x-4 lg:flex-1 lg:justify-start">
                 <li>Shop</li>
