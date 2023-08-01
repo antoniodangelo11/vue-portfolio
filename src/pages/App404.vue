417fa573171554b421d56ec06c8430fe4239c467
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-08-01 12:16:57 +0200

    add route App404 and content

---
+++ src/pages/App404.vue
@@ -0,0 +1,33 @@
+<script></script>
+
+<template>
+  <div
+    class="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16"
+  >
+    <div class="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
+      <div class="relative">
+        <div class="absolute">
+          <h1 class="my-2 text-gray-800 font-bold text-2xl">
+            Sembra che tu ti sia perso
+          </h1>
+          <p class="my-2 pb-4 text-gray-800">
+            La pagina che stai cercando non è disponibile!
+          </p>
+          <router-link
+            :to="{ name: 'home' }"
+            class="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50"
+            >Torna alla Home!</router-link
+          >
+        </div>
+        <div>
+          <img src="https://i.ibb.co/G9DC8S0/404-2.png" />
+        </div>
+      </div>
+    </div>
+    <div>
+      <img src="https://i.ibb.co/ck1SGFJ/Group.png" />
+    </div>
+  </div>
+</template>
+
+<style></style>
--- src/router.js
+++ src/router.js
@@ -1,28 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
-import Home from './pages/Home.vue';
-import About from './pages/About.vue';
-import Contacts from './pages/Contacts.vue';
+import Home from "./pages/Home.vue";
+import About from "./pages/About.vue";
+import Contacts from "./pages/Contacts.vue";
+import App404 from "./pages/App404.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-      {
-        path: '/',
-        name: 'home',
-        component: Home,
-      },
-      {
-        path: '/about-us',
-        name: 'about-us',
-        component: About,
-      },
-      {
-        path: '/contacts',
-        name: 'contacts',
-        component: Contacts,
-      }
-    ]
-  });
-  
-  export { router };
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/about-us",
+      name: "about-us",
+      component: About,
+    },
+    {
+      path: "/contacts",
+      name: "contacts",
+      component: Contacts,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "app404",
+      component: App404,
+    },
+  ],
+});
+
+export { router };
