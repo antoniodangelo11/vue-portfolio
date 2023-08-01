5d7e4e01b33c2a21f6504b5fb6ab54f0924d7753
DavideFarci <yank92.df@gmail.com>
2023-08-01 13:05:31 +0200

    remove card projects add card developer

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,35 +1,27 @@
-<script setup>
-
-</script>
+<script></script>
 
 <template>
-    <div class="container m-auto flex justify-center">
-        <div class="grid grid-cols-4 gap-4">
-            <div class="card">
-            </div>
-            <div class="card">
-            </div>
-            <div class="card">
-            </div>
-            <div class="card">
-            </div>
-            <div class="card">
-            </div>
-            <div class="card">
-            </div>
-            <div class="card">
-            </div>
-            <div class="card">
-            </div>
+  <div class="container mx-auto py-12">
+    <h1 class="text-center text-3xl font-extrabold mb-7">
+      Welcome in our Portfolios
+    </h1>
+    <div
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-3 md:p-0 lg:p-0"
+    >
+      <a v-for="index in 6" href="#">
+        <div class="card group">
+          <img
+            class="group-hover:object-none"
+            src="../../public/img/web-developer-portfolio.png"
+            alt="img"
+          />
+          <div class="title text-lg font-bold text-center">
+            Developer N° {{ index }}
+          </div>
         </div>
+      </a>
     </div>
+  </div>
 </template>
 
-<style lang="scss" scoped>
-.card {
-    width: 300px;
-    height: 500px;
-    border: 2px solid black;
-    margin: 2rem;
-}
-</style>
+<style lang="scss" scoped></style>
