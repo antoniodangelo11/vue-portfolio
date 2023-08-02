1a023dbcec7cfb3337e8c9fb173a76c31b8b5d6e
DavideFarci <yank92.df@gmail.com>
2023-08-02 18:13:09 +0200

    add data and paginator

--- src/pages/AppIndex.vue
+++ src/pages/AppIndex.vue
@@ -1,7 +1,35 @@
 <script>
+import axios from "axios";
 import AppCardProject from "../components/AppCardProject.vue";
 
-export default { components: { AppCardProject } };
+export default {
+  data() {
+    return {
+      arrProjects: [],
+      nPages: 0,
+    };
+  },
+  methods: {
+    getProjects() {
+      // this.loader = true;
+      axios
+        .get("http://localhost:8000/api/projects", {
+          params: {
+            page: this.currentPage,
+            // se sto già in prjects.index non esegue il craeated e non aggiorna la pagina
+            // q: new URLSearchParams(window.location.search).get("q"),
+          },
+        })
+        .then((response) => {
+          this.arrProjects = response.data.results.data;
+          this.nPages = response.data.results.last_page;
+          // this.loader = false;
+        });
+    },
+  },
+  create() {},
+  components: { AppCardProject },
+};
 </script>
 <template>
   <div
@@ -9,5 +37,92 @@ export default { components: { AppCardProject } };
   >
     <AppCardProject v-for="index in 6" :key="index" :dataCard="index" />
   </div>
+
+  <nav aria-label="Page navigation example">
+    <ul class="flex items-center -space-x-px h-10 text-base">
+      <li>
+        <a
+          href="#"
+          class="flex items-center justify-center px-4 h-10 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+        >
+          <span class="sr-only">Previous</span>
+          <svg
+            class="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 1 1 5l4 4"
+            />
+          </svg>
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >1</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >2</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          aria-current="page"
+          class="z-10 flex items-center justify-center px-4 h-10 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+          >3</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >4</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >5</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+        >
+          <span class="sr-only">Next</span>
+          <svg
+            class="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m1 9 4-4-4-4"
+            />
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
 <style lang="scss" scoped></style>
