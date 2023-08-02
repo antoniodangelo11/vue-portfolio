24dfbf3f7a5cbbdb39e4d017bdc7fa0e7dfa79c3
Paolo96700 <paolo.falcoapp@gmail.com>
2023-08-02 19:33:12 +0200

    mod - contacts

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -50,52 +50,75 @@ export default {
 </script>
 
 <template>
-  
-  <div class="container">
-    <h1 class="text-3xl font-bold mb-6">Contact Us</h1>
-
-    <div v-if="hasError" class="bg-red-100 text-red-600 border border-red-600 py-2 px-4 rounded mb-4">
-      Error in form submission!
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close" @click="hasError = false">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-
-    <div v-if="showSuccess" class="bg-green-100 text-green-600 border border-green-600 py-2 px-4 rounded mb-4">
-      Message sent successfully!
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close" @click="showSuccess = false">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-
-    <form @submit.prevent="sendLead" novalidate>
-      <div class="mb-4">
-        <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
-        <input type="name" class="form-input mt-1 block w-full rounded-md border-gray-300" id="name" v-model="name">
-      </div>
-
-      <div class="mb-4">
-        <label for="email" class="block text-sm font-medium text-gray-700">Email address</label>
-        <input type="email" class="form-input mt-1 block w-full rounded-md border-gray-300" id="email" v-model="email">
-      </div>
-
-      <div class="mb-4">
-        <label for="message" class="block text-sm font-medium text-gray-700">Message</label>
-        <textarea id="message" class="form-textarea mt-1 block w-full rounded-md border-gray-300" rows="5" v-model="message"></textarea>
-      </div>
-
-      <div class="mb-4">
-        <input type="checkbox" class="form-checkbox" id="newsletter" v-model="newsletter">
-        <label class="ml-2 text-sm text-gray-700" for="newsletter">Subscribe to newsletter</label>
-      </div>
-
-      <button type="submit" class="btn btn-primary" :class="{'opacity-50 pointer-events-none': isSending}">
-        Submit
-      </button>
-    </form>
+  <div v-if="hasError" class="m-4">
+    Error in form submission!
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close" @click="hasError = false">
+      <span aria-hidden="true">&times;</span>
+    </button>
   </div>
 
+  <div v-if="showSuccess" class="bg-green-100 text-green-600 border border-green-600 py-2 px-4 rounded mb-4">
+    Message sent successfully!
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close" @click="showSuccess = false">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
 
+  <section class="m-5 p-4 dark:text-gray-50">
+    <div class="p-5 text-gray-100">
+
+      <form @submit.prevent="sendLead" novalidate class="container flex flex-col mx-auto space-y-12">
+
+        <fieldset class="bg-slate-600 grid grid-cols-4 gap-4 p-6 rounded-md shadow-sm dark:bg-gray-900">
+          <h1 class="text-3xl font-bold mx-5">Contact Us</h1>
+        </fieldset>
+
+
+        <fieldset class="bg-slate-600 py-10 grid grid-cols-4 gap-4 px-6 rounded-md shadow-sm dark:bg-gray-900">
+
+          <div class="grid grid-cols-6 gap-4 col-span-full lg:col-span-6 ">
+            <div class="col-span-full sm:col-span-6">
+              <label for="name" class="block text-sm font-medium text-gray-100">Name</label>
+              <input type="name" class="form-input mt-1 block w-full rounded-md border-gray-300" id="name" v-model="name">
+            </div>
+          </div>
+
+
+          <div class="grid grid-cols-6 gap-4 col-span-full lg:col-span-6">
+            <div class="col-span-full sm:col-span-6">
+              <label for="email" class="block text-sm font-medium text-gray-100">Email address</label>
+              <input type="email" class="form-input mt-1 block w-full rounded-md border-gray-300" id="email"
+                v-model="email">
+            </div>
+
+          </div>
+
+          <div class="grid grid-cols-6 gap-4 col-span-full lg:col-span-6">
+            <div class="col-span-full sm:col-span-6">
+              <label for="message" class="block text-sm font-medium text-gray-100">Message</label>
+              <textarea id="message" class="form-textarea mt-1 block w-full rounded-md border-gray-300" rows="5"
+                v-model="message"></textarea>
+            </div>
+
+          </div>
+
+          <div>
+            <input type="checkbox" class="form-checkbox" id="newsletter" v-model="newsletter">
+            <label class="ml-2 text-sm text-gray-100" for="newsletter">Subscribe to newsletter</label>
+          </div>
+        </fieldset>
+
+        <div class="bg-slate-600 py-4 flex justify-center rounded-md shadow-sm dark:bg-gray-900">
+          <button type="submit" class="px-4 py-2 border rounded-md dark:border-gray-100"
+            :class="{ 'opacity-50 pointer-events-none': isSending }">
+            Submit
+          </button>
+        </div>
+
+      </form>
+    </div>
+
+  </section>
 </template>
 
 <style lang="scss" scoped></style>
