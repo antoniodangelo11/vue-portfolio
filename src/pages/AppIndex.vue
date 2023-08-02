4fd2609d52390aa3aec3c6c1a0a3ec1be9e07437
DavideFarci <yank92.df@gmail.com>
2023-08-02 19:30:57 +0200

    remove padding bottom in container

--- src/pages/AppIndex.vue
+++ src/pages/AppIndex.vue
@@ -57,12 +57,17 @@ export default {
   created() {
     this.getProjects();
   },
+  watch: {
+    currentPage() {
+      this.getProjects();
+    },
+  },
   components: { AppCardProject },
 };
 </script>
 <template>
   <div
-    class="lg:container lg:mx-auto grid grid-cols-1 lg:grid-cols-2 px-4 md:px-6 lg:px-4 gap-10 py-4 pb-40"
+    class="lg:container lg:mx-auto grid grid-cols-1 lg:grid-cols-2 px-4 md:px-6 lg:px-4 gap-10 py-4"
   >
     <AppCardProject
       v-for="project in arrProjects"
