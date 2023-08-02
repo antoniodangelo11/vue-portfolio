b2e9ce1ff2e40253676e3118781e24bb57389b20
DavideFarci <yank92.df@gmail.com>
2023-08-03 01:15:22 +0200

    add userId and mutation

--- src/store.js
+++ src/store.js
@@ -1,5 +1,10 @@
-import {reactive} from 'vue';
+import { reactive } from "vue";
 
 export const store = reactive({
-    baseUrl: 'http://localhost:8000/',
-})
+  baseUrl: "http://localhost:8000/",
+  userProjects: [],
+  userId: null,
+  setUserId(index) {
+    store.userId = index;
+  },
+});
