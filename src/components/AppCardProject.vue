0333ac892c947bbb62fdd26732456258ad09ac53
DavideFarci <yank92.df@gmail.com>
2023-08-06 22:31:15 +0200

    fix padding of author name in cards

--- src/components/AppCardProject.vue
+++ src/components/AppCardProject.vue
@@ -47,7 +47,7 @@ export default {
         </div>
         <div class="author inline-block">
           <div
-            class="bg-blue-700 text-white border border-blue-300 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group inline-block"
+            class="bg-blue-700 text-white border border-blue-300 border-b-4 font-medium overflow-hidden relative px-4 py-1 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group inline-block"
           >
             <span
               class="bg-blue-300 shadow-blue-300 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"
