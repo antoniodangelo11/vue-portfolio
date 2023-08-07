c8db887ad3b2450ae3277f4c384a8fc4eb89c6da
Paolo96700 <paolo.falcoapp@gmail.com>
2023-08-07 18:15:18 +0200

    fix-video

--- src/components/AppCardProject.vue
+++ src/components/AppCardProject.vue
@@ -1,10 +1,13 @@
 <script>
+import { store } from "../store";
+
 export default {
   props: {
     dataCard: Object,
   },
   data() {
     return {
+      store,
       isPlaying: false,
     };
   },
@@ -24,15 +27,9 @@ export default {
   <div class="card-project drop-shadow-lg rounded-md overflow-hidden">
     <div @mouseenter="playVideo" @mouseleave="pauseVideo" class="relative">
       <video ref="videoRef" class="w-full h-full object-cover" loop muted>
-        <source
-          src="../../public/img/spotify-responsive.mp4"
-          type="video/mp4"
-        />
+        <source :src="store.baseUrl + `storage/uploads` + dataCard.video" type="video/webm" />
       </video>
-      <div
-        v-if="!isPlaying"
-        class="absolute inset-0 flex items-center justify-center"
-      ></div>
+      <div v-if="!isPlaying" class="absolute inset-0 flex items-center justify-center"></div>
     </div>
     <div class="text bg-slate-400 py-3 px-4">
       <div class="title text-3xl font-bold mb-6">{{ dataCard.title }}</div>
@@ -47,26 +44,18 @@ export default {
           {{ dataCard.user.name }}
         </div> -->
         <div
-          class="bg-blue-500 text-white border border-blue-300 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group inline-block"
-        >
+          class="bg-blue-500 text-white border border-blue-300 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group inline-block">
           <span
-            class="bg-blue-300 shadow-blue-300 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"
-          ></span>
+            class="bg-blue-300 shadow-blue-300 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
           {{ dataCard.user.name }}
         </div>
       </div>
       <div class="technologies py-2">
-        <span
-          v-for="technology in dataCard.technologies"
-          class="text-slate-700 hover:cursor-pointer hover:text-black mr-2"
-          >{{ technology.name }}</span
-        >
+        <span v-for="  technology   in   dataCard.technologies  "
+          class="text-slate-700 hover:cursor-pointer hover:text-black mr-2">{{ technology.name }}</span>
       </div>
       <div class="types">
-        <span
-          class="text-slate-700 hover:cursor-pointer hover:text-black mr-2"
-          >{{ dataCard.type.name }}</span
-        >
+        <span class="text-slate-700 hover:cursor-pointer hover:text-black mr-2">{{ dataCard.type.name }}</span>
       </div>
     </div>
   </div>
